ad5c68bd2fe908dadb448f79c97e31957389b28f
BoneLord <mr.zombi@mail.ru>
2013-05-14 17:56:07 +0400

    Prepare testing class.

---
+++ test/skilltest.cpp
@@ -0,0 +1,5 @@
+#include "skilltest.h"
+
+SkillTest::SkillTest()
+{
+}
---
+++ test/skilltest.h
@@ -0,0 +1,11 @@
+#ifndef SKILLTEST_H
+#define SKILLTEST_H
+
+#include <gtest/gtest.h>
+
+class SkillTest : public ::testing::Test {
+public:
+    SkillTest();
+};
+
+#endif // SKILLTEST_H
